b4bd2a58a78a6ed092fe652b73781871ad06006c
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-09-13 23:31:09 +0600

    maxreward frontend init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return <div className="flex min-h-svh flex-col items-center justify-center bg-red-500">
+      <Button>Click me</Button>
+    </div>
+;
+};
+
+export default App;
