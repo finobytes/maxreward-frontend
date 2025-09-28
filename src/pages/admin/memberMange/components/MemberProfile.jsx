66ce387081eb472e0d22df61f5fbc5eb107e9b8e
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-09-28 09:11:16 +0600

    Add alt attributes for accessibility in MemberProfile component

--- src/pages/admin/memberMange/components/MemberProfile.jsx
+++ src/pages/admin/memberMange/components/MemberProfile.jsx
@@ -17,6 +17,7 @@ const MemberProfile = () => {
         <img
           src={profileCover}
           className="w-full xl:h-[9rem] lg:h-[9rem] md:h-[7rem] sm:h-[7rem] h-[5rem] rounded-sm"
+          alt="Profile cover"
         />
 
         {/* User Profile Image */}
@@ -24,41 +25,42 @@ const MemberProfile = () => {
           <img
             src={userProfile}
             className="rounded-full object-cover w-24 h-24 relative -top-12 left-6"
+            alt="user profile"
           />
         </div>
       </div>
-      <div className="ml-4 block 2xl:flex gap-4 2xl:justify-between relative -top-8">
-        <div>
+      <div className=" block 2xl:flex gap-4 2xl:justify-between relative -top-8">
+        <div className="ml-2">
           <h3 className="text-2xl font-semibold">Leo Philips</h3>
           <p className="mt-4 text-gray-400">Chief Executive Officer</p>
           <div className="flex gap-4 mt-1">
             <div className="flex gap-1">
-              <img src={cityIcon} />
+              <img src={cityIcon} alt="icon" />
               <span className="text-sm text-gray-400">Georgia</span>
             </div>
             <div className="flex gap-1">
-              <img src={locationIcon} />
+              <img src={locationIcon} alt="icon" />
               <span className="text-sm text-gray-400">Washington D.C</span>
             </div>
           </div>
         </div>
         <div className="flex flex-wrap justify-start items-center gap-4 mt-4 2xl:mt-0">
           <div className="border-2 border-gray-300 flex items-center gap-4 px-2 py-0.5 rounded-full">
-            <img className="w-10 h-10" src={bag2} />
+            <img className="w-10 h-10" src={bag2} alt="icon" />
             <div className="pr-3">
               <p>113</p>
               <span className="text-sm text-gray-600">Invited</span>
             </div>
           </div>
           <div className="border-2 border-gray-300 flex items-center gap-4 px-2 py-0.5 rounded-full">
-            <img className="w-10 h-10" src={users2} />
+            <img className="w-10 h-10" src={users2} alt="icon" />
             <div className="pr-3">
               <p>68</p>
               <span className="text-sm text-gray-600">Referrals</span>
             </div>
           </div>
           <div className="border-2 border-gray-300 flex items-center gap-4 px-2 py-0.5 rounded-full">
-            <img className="w-10 h-10" src={profile} />
+            <img className="w-10 h-10" src={profile} alt="icon" />
             <div className="pr-3">
               <p>$3.1K</p>
               <span className="text-sm text-gray-600">Purchased</span>
