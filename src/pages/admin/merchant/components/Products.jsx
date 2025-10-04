85bcb77d36e39ec23fafd2e5fde48ba6ff8dff29
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-10-04 13:09:36 +0600

    add statement

---
+++ src/pages/admin/merchant/components/Products.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="min-h-screen">
+      <h3 className="flex flex-col justify-center items-center"></h3>
+    </div>
+  );
+};
+
+export default Products;
---
+++ src/pages/admin/merchant/components/ReferredMember.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReferredMember = () => {
+  return <div>Referred Member</div>;
+};
+
+export default ReferredMember;
