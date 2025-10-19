5113c0cf159eef36757cf2472ad8d5aabd97fc4c
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-10-19 06:45:52 +0600

    fix spelling

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-white">
       <h1 className="text-5xl font-extrabold text-[#ff5a29] animate-apple-logo">
-        MaxReword
+        MaxReward
       </h1>
       <div className="mt-6 w-4 h-4 rounded-full bg-[#ff5a29] animate-ping opacity-70"></div>
     </div>
