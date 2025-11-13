db8bbcae1586582012a5871c34b35b40834d6db4
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-11-13 22:48:46 +0600

    member details inprogress

--- src/pages/admin/memberMange/components/ProfileTabContent.jsx
+++ src/pages/admin/memberMange/components/ProfileTabContent.jsx
@@ -1,11 +1,14 @@
 import CommunityTree from "./CommunityTree";
 import AddPayment from "./AddPayment";
 import Statements from "./Statements";
+import CommunityUplink from "./CommunityUplink";
 
 const ProfileTabContent = ({ currentTab }) => {
   switch (currentTab) {
     case "community":
       return <CommunityTree />;
+    case "communityUplink":
+      return <CommunityUplink />;
     case "statements":
       return <Statements />;
     default:
