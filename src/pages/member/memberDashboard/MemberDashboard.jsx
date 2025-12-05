e9610ba04475e305fb43611920a2348e7685e88a
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-12-05 18:27:11 +0600

    introduce dashboard pages and a reusable dashboard card component for admin, member, and merchant roles.

--- src/pages/member/memberDashboard/MemberDashboard.jsx
+++ src/pages/member/memberDashboard/MemberDashboard.jsx
@@ -122,7 +122,9 @@ const MemberDashboard = () => {
 
   return (
     <div>
-      <h1 className="text-xl font-semibold text-gray-600 pb-4">Dashboard</h1>
+      <h1 className="text-base md:text-lg lg:text-xl font-semibold text-gray-600 pb-4">
+        Dashboard
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4">
         {/* Membership Card should take 1 column on all screens, stays the same */}
         <div className="2xl:col-span-1">
@@ -131,7 +133,7 @@ const MemberDashboard = () => {
 
         {/* Dashboard Cards wrapper should take 2 columns on 2XL screens */}
         <div className="2xl:col-span-2">
-          <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-2 2xl:grid-cols-3 gap-4">
             {cardsData.map((card, index) => (
               <DashboardCard key={index} {...card} />
             ))}
